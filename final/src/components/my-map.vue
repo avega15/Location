40485b8d667cd425b501da66cbae4188274d58d0
Abe Vega <avega15@calstatela.edu>
2020-07-16 14:54:14 -0700

    first commit

---
+++ final/src/components/my-map.vue
@@ -0,0 +1,34 @@
+<template lang="html">
+
+  <section class="my-map">
+    <h1>my-map Component</h1>
+  </section>
+
+</template>
+
+<script lang="js">
+  export default  {
+    name: 'my-map',
+    props: [],
+    mounted() {
+
+    },
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+</script>
+
+<style scoped lang="scss">
+  .my-map {
+
+  }
+</style>
